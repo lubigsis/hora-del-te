3c4839b85d510d2db8c7c1a294827646446d14de
lubigsis <lucre.baluc@gmail.com>
2023-02-27 01:11:19 -0300

    Declaro una función con otras funciones internas

--- main.js
+++ main.js
@@ -5,18 +5,17 @@ window.onload = () => {
     let videoTacitaHumeando = document.getElementById('tacita-humeandoJS');
     let estadoTacita = 'vacia';
 
-//Aprieto el botón de servir.
+//Aprieto el botón de servir. Q cuando le hagan click al botonServir, ejecute una función; la cual x dentro, ejecutará otra función
 botonServir.onclick = () => {
-    //Fijarse si la taza está vacía.
-    if (estadoTacita == 'vacia'){
-    //Reproducir video de la tetera sirviendo.
-    reproducir(videoTacita);
-
-    videoTacita.onended = () =>{
-    //Ocultar video
-    ocultar(videoTacita);
-
-    
+ 
+    if (estadoTacita == 'vacia'){ //----1) Me fijo si la taza está vacía:   
+        reproducir(videoTacita);  //--Si está vacía, q' reproduzca el video y q' luego lo oculte, etc.
+        videoTacita.onended = () =>{
+            ocultar(videoTacita);
+            mostrar(videoTacitaHumeando);
+            reproducir(videoTacitaHumeando);
+            resetear(videoTacita);
+            estadoTacita('llena');                //Declarar estas funciones
     }
 
     }
@@ -24,6 +23,34 @@ botonServir.onclick = () => {
 
 
 }
+
+/*----------------------------------------------------------------Declaro FUNCIONES-----------------*/
+function reproducir(video, loopear) {  //-----------------Le da play al video
+    if (loopear == 'loopear') { //---si loopear viene c/ el valor de loopear, entonces video.loop= true (se frenaba el humo)
+        video.loop = true;
+    }
+    video.play();
+}
+
+
+function ocultar(video) {
+    video.classList.add('display-none'); //----Clase q' puse en CSS p/ ocultar el video
+}
+
+
+function mostrar (video) {
+    video.classList.remove('display-none'); //---solo saca la clase 'display-none'
+}
+
+function resetear (video) { //---rebobino: q' corra la línea de tiempo al segundo 0
+    video.pause();
+    video.currentTime = 0;
+}
+
+
+
+/*----------------------------------------------------------------------------------------------------------------*/
+
 
 /*
 Lo que hay que hacer:
